9f622b1e0cc025d1efcd96bbf0ecd3706535bd4c
Dormant <dormantman@yandex.ru>
2017-11-28 20:25:53 +0300

    Create 2. Tiles.py

---
+++ Municipal stage 2017-2018/2. Tiles.py
@@ -0,0 +1,27 @@
+inp = open('input.txt', 'r')
+def input(): return inp.readline()
+out = open('output.txt', 'w')
+def print(s): return out.write(str(s))
+
+m, n = map(int, input().split())
+k = int(input())
+
+root = []
+
+for i in range(k):
+    a, p = map(int, input().split())
+    root.append((p, a))
+    
+res = 1024**256
+    
+for i in sorted(root):
+    r = i[1]
+    if m%r==0 and n%r==0:
+        yet = i[0]*(m//r)*(n//r)
+        if yet < res:
+            res = yet
+    
+if res == 1024**256:
+    print('no')
+else:
+    print(res)
